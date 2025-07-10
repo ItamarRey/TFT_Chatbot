669089545090c4a9621fe49a685f10c6d2d0c18d
Itamar <itamarreyrguez@gmail.com>
2025-07-10 19:28:59 +0100

    Repository initialized

---
+++ plocabot_ui/config.js
@@ -0,0 +1,12 @@
+const CONFIG = {
+  botName: "AnfiBot",
+  companyName: "QAISC",
+  urlPath: "https://qaisc.com",
+  logoPath: "data_images/qaisc_logo_left.png",
+  iconPath: "data_images/qaisc_icon.png",
+  iconRegisterPath: "/plocabot_ui/data_images/qaisc_icon.png",
+  iconRegisterFooterPath: "/plocabot_ui/data_images/qaisc_logo.png",
+  botTextImage: "data_images/qaisc_logo2.png",
+  botBackground: "data_images/qaisc_background.jpg",
+  registerTitle: "¡Bienvenido a AnfiBot!"
+};
